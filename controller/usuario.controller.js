7ec87f5ab35b1f0a8016f307be57639c6ee2bce6
Marcio <marcioxerfan@gmail.com>
2023-04-10 18:13:42 -0300

    First commit

---
+++ controller/usuario.controller.js
@@ -0,0 +1,123 @@
+const usuarios = [
+    {
+        id: 1,
+        nome: "usuario 1",
+        fName: "Lion",
+        lName: "Brr"
+    },
+    {
+        id: 2,
+        nome: "usuario 2",
+        fName: "Cow",
+        lName: "Mmm"
+    },
+    {
+        id: 3,
+        nome: "usuario 3",
+        fName: "Cat",
+        lName: "Miu"
+    },
+]
+
+const find = (req, res) => {
+    const id = req.params.id;
+    let found = false;
+
+    usuarios.map(function (valor) {
+        if (valor.id == id) {
+            found = true;
+            return res.send(valor);
+        }
+    });
+
+    if(!found){
+        res.status(404).send({message: "Não foi encontrado"});
+    }
+}
+
+const findAllUsuarios = (req,res) => {
+    res.send(usuarios);
+}
+
+const createUsuario = (req,res) => {
+    const usuario = req.body;
+    
+    if(Object.keys(usuario).length === 0){
+        return res.status(400).send({message: "O corpo da mensagem está vazio"});
+    }
+
+    if(!usuario.id){
+        return res.status(400).send({message: "O campo 'id' não foi encontrado!"})
+    }
+
+    if(!usuario.nome){
+        return res.status(400).send({message: "O campo 'nome' não foi encontrado!"})
+    }
+
+    if(!usuario.numFuncionarios){
+        return res.status(400).send({message: "O campo 'numFuncionarios' não foi encontrado!"})
+    }
+
+    usuario.nacionalidade = "brasileira";
+
+    usuarios.push(usuario);
+    res.status(201).send(usuarios);
+}
+const updateUsuario = (req, res) =>{
+    const id = req.params.id;
+    const usuario = req.body;
+    let found = false;
+
+    if(Object.keys(usuario).length === 0){
+        return res.status(400).send({message: "O corpo da mensagem está vazio"});
+    }
+
+    if(!usuario.id){
+        return res.status(400).send({message: "O campo 'id' não foi encontrado!"})
+    }
+
+    if(!usuario.nome){
+        return res.status(400).send({message: "O campo 'nome' não foi encontrado!"})
+    }
+
+    if(!usuario.numFuncionarios){
+        return res.status(400).send({message: "O campo 'numFuncionarios' não foi encontrado!"})
+    }
+
+    usuarios.map( function (valor, index){
+        if (valor.id == id) {
+            found = true;
+            usuarios[index] = usuario;
+            return res.send(usuarios[index]);
+        }
+    });
+
+    if (!found){
+        res.status(404).send({message: "Não foi encontrado"});
+    }
+} 
+
+const deleteUsuario = (req,res) => {
+    const id = req.params.id;
+    let found = false;
+
+    usuarios.map(function (valor, index) {
+        if (valor.id == id) {
+            found = true;
+            usuarios.splice(index, 1);
+            return res.send(valor);
+        }
+    });
+
+    if(!found){
+        res.status(404).send({message: "Não foi encontrado"});
+    }
+}
+
+module.exports = {
+    find,
+    findAllUsuarios,
+    createUsuario,
+    updateUsuario,
+    deleteUsuario
+}
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const usuario = require("./router/usuario.router");
+
+const port = 3000;
+
+app.use(express.json());
+
+app.use("/usuario", usuario);
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.get("/contato", (req, res) => {
+    res.send("Nosso contato: email@email.com");
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
+
